933ced8c7091ec07c76eeed96fc08d4084b9f008
dharmapal25 <dharmapalbharati2525@gmail.com>
2026-06-14 10:12:31 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173" || "https://flashgpt-ai.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
